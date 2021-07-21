91f4cece3dd3e895151d047ae9a7d051b79eed16
Pawan Kumar <pawankumar@madebyaura.net>
2021-07-21 14:59:27 +0530

    check status

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
 
 function aura_template_parts($slug, $args){
+	$status = apply_filters( 'aura_theme_template_part_status', true, $slug);
+
+	if($status === false){
+		return;
+	}
+
 	$args = apply_filters( 'aura_theme_template_part_tag', $args, $slug );
 
 	do_action( 'aura_theme_template_part_before', $slug, $args );
@@ -19,4 +25,12 @@ function my_filter( $args, $slug ){
 		$args['text'] = 'pawan';
 	}
 		return $args;
+}
+
+add_filter('aura_theme_template_part_status', 'remove_header', 10, 2);
+function remove_header( $status, $slug ){
+	if( $slug === 'template-parts/header' && is_singular( [ 'post', 'page'] )){
+		$status = false;
+	}
+	return $status;
 }
